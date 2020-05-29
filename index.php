85f6cd3a8a0db01ed2599dc09af3afb330291f05
wayanedi <37069217+wayanedi@users.noreply.github.com>
2020-05-29 15:54:28 +0700

    Update index.php

--- index.php
+++ index.php
@@ -45,7 +45,7 @@
 			</div>
 
 			<div class="element-form">
-				<span><input type="submit" name="submit" id="submit" value="LOGIN"></span>
+				<span><input type="submit" name="submit" id="submit" value="LOGIN GAN"></span>
 			</div>
 		</div>	
 	</form>
